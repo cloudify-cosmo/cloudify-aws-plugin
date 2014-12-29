f799d02f89c51a681ac3b5974931b922d7f4b397
earthmant <trammell.earthling@gmail.com>
2014-12-29 14:18:29 +0200

    added set_instance_state, state_equals_state, poll_for_state

--- ec2/instance.py
+++ ec2/instance.py
@@ -13,6 +13,9 @@
 #    * See the License for the specific language governing permissions and
 #    * limitations under the License.
 
+# general imports
+import time
+
 # ctx is imported and used in operations
 from cloudify import ctx
 from cloudify.exceptions import NonRecoverableError
@@ -21,8 +24,16 @@ from cloudify.exceptions import NonRecoverableError
 from cloudify.decorators import operation
 
 # boto imports
-from boto.ec2 import EC2Connection
+from boto.ec2 import EC2Connection as EC2
 from boto.exception import EC2ResponseError
+
+# EC2 Instance States
+INSTANCE_PENDING = 0
+INSTANCE_RUNNING = 16
+INSTANCE_SHUTTING_DOWN = 32
+INSTANCE_TERMINATED = 48
+INSTANCE_STOPPING = 64
+INSTANCE_STOPPED = 80
 
 
 @operation
@@ -35,11 +46,11 @@ def run(**kwargs):
     instance_type = ctx.node.propertes['instance_type']
 
     try:
-        reservation = EC2Connection().run_instances(image_id=ami_image_id, instance_type=instance_type)
+        reservation = EC2().run_instances(image_id=ami_image_id, instance_type=instance_type)
+        ctx.node.runtime_properties['reservation'] = reservation
+        return True
     except EC2ResponseError as e:
         raise NonRecoverableError(e.body)
-
-    return reservation
 
 
 def start():
@@ -56,3 +67,66 @@ def terminate():
 
 def creation_validation():
     return True
+
+def set_instance_state(instance):
+    """
+    :param instance: an instance in reservation.instances
+    :return:
+    """
+
+    state = EC2().get_all_instance_status(instance_ids=instance.id)[0]
+    ctx.node.runtime_properties['{0}_state_name'.format(instance.id)] = state.state_name
+    ctx.node.runtime_properties['{0}_state_code'.format(instance.id)] = state.state_code
+
+
+def set_instance_states():
+    """ The above method for many instances.
+    :return:
+    """
+    reservation = ctx.node.runtime_properties['reservation']
+    for instance in reservation.instances:
+        set_instance_state(instance)
+
+
+def state_equals_state(instance, state):
+    """
+    :param instance: an instance in reservation.instances
+    :param state: a state from EC2 Instance States
+    :return: True if the states are equal, otherwise False.
+    """
+    instance_state = ctx.node.runtime_properties['{0}_state_code'.format(instance.id)]
+
+    if instance_state == state:
+        return True
+    else:
+        return False
+
+def poll_for_state(instance, state=INSTANCE_RUNNING, timeout=30):
+    """
+    :param instance: an instance in reservation.instances
+    :param state: a state from the EC2 Instance States
+    :param timeout: How long you want to try for (in secs).
+    :return:
+    """
+
+    outcome = False
+
+    while True:
+        set_instance_state(instance)
+        if state_equals_state(instance, state):
+            outcome = True
+            break
+        elif time.time() > timeout:
+            break
+
+    return outcome
+
+def poll_for_states(state=INSTANCE_RUNNING, timeout=30):
+    """ The above method for many instances.
+    :param state: a state from the EC2 Instance States
+    :param timeout: How long you want to try for (in secs).
+    :return:
+    """
+    reservation = ctx.node.runtime_properties['reservation']
+    for instance in reservation.instances:
+        poll_for_state(instance, state, timeout)
