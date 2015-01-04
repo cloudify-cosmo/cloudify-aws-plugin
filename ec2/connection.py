236315ca76ce7122069acc8ab02b4bdff3382b54
earthmant <trammell.earthling@gmail.com>
2015-01-04 16:54:59 +0200

    removed @operation tags, connect_to_region

--- ec2/connection.py
+++ ec2/connection.py
@@ -13,17 +13,12 @@
 #    * See the License for the specific language governing permissions and
 #    * limitations under the License.
 
-# built-in package
-
 # other packages
 from boto.ec2 import EC2Connection
-from boto.ec2 import connect_to_region
 from boto.exception import EC2ResponseError
 
-# ctx packages
-from cloudify import ctx
+# Cloudify packages
 from cloudify.exceptions import NonRecoverableError
-from cloudify.decorators import operation
 
 
 class EC2Client():
@@ -31,32 +26,11 @@ class EC2Client():
     def __init__(self):
         self.connection = None
 
-    @operation
-    def connect(self, **kwargs):
-
-        ctx.logger.info('(Connecting to AWS EC2.')
+    def connect(self):
 
         try:
             self.connection = EC2Connection()
         except EC2ResponseError:
-            ctx.logger.error("""Failed to connect to EC2: API returned: {0}."""
-                             .format(EC2ResponseError))
-            raise NonRecoverableError("""Error. Failed to connect to EC2:
-                                         API returned: {0}."""
-                                      .format(EC2ResponseError))
-
-        return self.connection
-
-    @operation
-    def connect_to_region(self, region, **kwargs):
-
-        ctx.logger.info('(Connecting to AWS EC2.')
-
-        try:
-            self.connection = connect_to_region(region)
-        except EC2ResponseError:
-            ctx.logger.error("""Failed to connect to EC2: API returned: {0}."""
-                             .format(EC2ResponseError))
             raise NonRecoverableError("""Error. Failed to connect to EC2:
                                          API returned: {0}."""
                                       .format(EC2ResponseError))
