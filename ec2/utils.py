67abb29ff2dda0a863ed43a68586c9165f3fd267
earthmant <trammell.earthling@gmail.com>
2015-01-14 13:46:14 +0200

    added the save keypair func

--- ec2/utils.py
+++ ec2/utils.py
@@ -120,3 +120,21 @@ def get_instance_from_id(instance_id, ctx):
     instance = reservations[0].instances[0]
 
     return instance
+
+
+def save_key_pair(key_pair_object, ctx):
+    """ Saves the key pair to the file specified in the blueprint
+    """
+
+    ctx.logger.debug('Attempting to save the key_pair_object.')
+
+    try:
+        key_pair_object.save(ctx.node.properties['private_key_path'])
+    except OSError:
+        raise NonRecoverableError('Unable to save key pair to file: {0}.'
+                                  'OS Returned: {1}'.format(
+                                      ctx.node.properties['private_key_path'],
+                                      OSError))
+
+    ctx.logger.debug('No errors were raised while saving the '
+                     ' key_pair_object.')
