ba120d3685c686403df8546b29994d27b33aa4ca
earthmant <trammell.earthling@gmail.com>
2015-03-06 15:22:12 +0200

    firt

---
+++ ec2/constants.py
@@ -0,0 +1,20 @@
+########
+# Copyright (c) 2014 GigaSpaces Technologies Ltd. All rights reserved
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#        http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+#    * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#    * See the License for the specific language governing permissions and
+#    * limitations under the License.
+
+EXTERNAL_RESOURCE_ID = 'aws_resource_id'
+
+INSTANCE_STATE_STARTED = 16
+INSTANCE_STATE_TERMINATED = 48
+INSTANCE_STATE_STOPPED = 80
