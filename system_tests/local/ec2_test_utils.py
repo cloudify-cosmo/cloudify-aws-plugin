5174f32bcb807f2f63a5a25fdaf004b8202a9aa9
earthmant <trammell.earthling@gmail.com>
2015-03-17 15:45:33 +0200

    first commit

---
+++ system_tests/local/ec2_test_utils.py
@@ -0,0 +1,98 @@
+########
+# Copyright (c) 2014 GigaSpaces Technologies Ltd. All rights reserved
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#        http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+#    * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#    * See the License for the specific language governing permissions and
+#    * limitations under the License.
+
+# Built-in Imports
+import os
+import testtools
+
+# Third party Imports
+from boto.ec2 import EC2Connection
+
+# Cloudify Imports
+from cloudify.workflows import local
+
+IGNORED_LOCAL_WORKFLOW_MODULES = (
+    'worker_installer.tasks',
+    'plugin_installer.tasks'
+)
+
+TEST_AMI = 'ami-3cf8b154'
+TEST_SIZE = 'm3.medium'
+EXTERNAL_RESOURCE_ID = 'aws_resource_id'
+SIMPLE_IP = 'simple_elastic_ip'
+SIMPLE_SG = 'simple_security_group'
+SIMPLE_KP = 'simple_key_pair'
+SIMPLE_VM = 'simple_instance'
+PAIR_A_IP = 'pair_a_connected_elastic_ip'
+PAIR_A_VM = 'pair_a_connected_instance'
+
+
+class EC2LocalTestUtils(testtools.TestCase):
+
+    def setUp(self):
+        super(EC2LocalTestUtils, self).setUp()
+        self._set_up()
+
+    def tearDown(self):
+        super(EC2LocalTestUtils, self).tearDown()
+
+    def _set_up(self,
+                inputs=None,
+                directory='resources',
+                filename='blueprint.yaml'):
+
+        blueprint_path = os.path.join(
+            os.path.dirname(
+                os.path.dirname(__file__)), directory, filename)
+
+        if not inputs:
+            inputs = self._get_inputs(TEST_AMI, TEST_SIZE)
+
+        # setup local workflow execution environment
+        self.env = local.init_env(
+            blueprint_path,
+            name=self._testMethodName,
+            inputs=inputs,
+            ignored_modules=IGNORED_LOCAL_WORKFLOW_MODULES)
+
+    def _get_inputs(self,
+                    ami_image_id,
+                    instance_type,
+                    test_name='vanilla_test'):
+
+        return {
+            'image': ami_image_id,
+            'size': instance_type,
+            'key_path': '~/.ssh/{0}.pem'.format(test_name)
+        }
+
+    def _get_instances(self, storage):
+        return storage.get_node_instances()
+
+    def _get_instance_node(self, node_name, storage):
+        for instance in self._get_instances(storage):
+            if node_name in instance.node_id:
+                return instance
+
+    def _get_instance_node_id(self, node_name, storage):
+        instance_node = self._get_instance_node(node_name, storage)
+        return instance_node.node_id
+
+    def _get_ec2_client(self):
+        return EC2Connection()
+
+    def _create_security_group(self, ec2_client, name, description):
+        new_group = ec2_client.create_security_group(name, description)
+        return new_group
---
+++ system_tests/local/test_plugin_local_workflows.py
@@ -0,0 +1,99 @@
+########
+# Copyright (c) 2014 GigaSpaces Technologies Ltd. All rights reserved
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#        http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+#    * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#    * See the License for the specific language governing permissions and
+#    * limitations under the License.
+
+# Built-in Imports
+import os
+
+# Cloudify Imports
+from ec2_test_utils import (
+    EC2LocalTestUtils,
+    EXTERNAL_RESOURCE_ID,
+    SIMPLE_IP, SIMPLE_SG, SIMPLE_KP, SIMPLE_VM,
+    PAIR_A_IP, PAIR_A_VM
+)
+
+TEST_AMI = 'ami-3cf8b154'
+TEST_SIZE = 'm3.medium'
+
+
+class TestWorkflowClean(EC2LocalTestUtils):
+
+    def test_simple_resources(self):
+        """ Tests the install workflow using the built in
+            workflows.
+        """
+
+        test_name = 'test_simple_resources'
+
+        inputs = self._get_inputs(TEST_AMI, TEST_SIZE, test_name)
+        self._set_up(inputs=inputs)
+
+        # execute install workflow
+        self.env.execute('install', task_retries=10)
+
+        instance_storage = self._get_instances(self.env.storage)
+
+        self.assertEquals(self.expected_nodes, len(instance_storage))
+
+        for node_instance in self._get_instances(self.env.storage):
+            self.assertIn(EXTERNAL_RESOURCE_ID,
+                          node_instance.runtime_properties)
+
+        # Test assertions for simple nodes
+        self.assertIsNotNone(
+            self._get_instance_node_id(
+                SIMPLE_IP, self.env.storage))
+
+        self.assertIsNotNone(
+            self._get_instance_node_id(
+                SIMPLE_SG, self.env.storage))
+
+        self.assertIsNotNone(
+            self._get_instance_node_id(
+                SIMPLE_KP, self.env.storage))
+
+        self.assertIsNotNone(
+            self._get_instance_node_id(
+                SIMPLE_VM, self.env.storage))
+
+        # Test assertions for pair a nodes
+        self.assertIsNotNone(
+            self._get_instance_node_id(
+                PAIR_A_IP, self.env.storage))
+
+        self.assertIsNotNone(
+            self._get_instance_node_id(
+                PAIR_A_VM, self.env.storage))
+
+        pair_a_vm_instance = \
+            self._get_instance_node(PAIR_A_VM, self.env.storage)
+
+        self.assertEquals(1, len(pair_a_vm_instance.relationships))
+
+        self.env.execute('uninstall', task_retries=10)
+
+        instance_storage = self._get_instances(self.env.storage)
+
+        self.assertEquals(self.expected_nodes, len(instance_storage))
+
+        key_pair_file = \
+            os.path.join(os.path.expanduser('~/.ssh'), test_name, '.pem')
+
+        if os.path.exists(key_pair_file):
+            os.remove(key_pair_file)
+
+    @property
+    def expected_nodes(self):
+        return 6
