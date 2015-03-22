d3f408927ee6bd236421cab7f87c876f7d6e132b
earthmant <earthmant@icloud.com>
2015-03-22 10:33:59 +0200

    cleaned up. all working

--- system_tests/local/test_plugin.py
+++ system_tests/local/test_plugin.py
@@ -234,8 +234,6 @@ class EC2UtilsUnitTests(EC2LocalTestUtils):
         ctx.node.properties['private_key_path'] = \
             '~/.ssh/test_utils_get_resource_id_from_key_path.pem'
 
-        ctx.node.properties['resource_id']
-
         resource_id = utils.get_resource_id(ctx=ctx)
 
         self.assertEquals(
