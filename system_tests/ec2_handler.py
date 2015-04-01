cc0a6ba0a2652903896554d487c6df1981fffd42
earthmant <earthmant@icloud.com>
2015-04-01 17:56:41 +0300

    changing the path here

--- system_tests/ec2_handler.py
+++ system_tests/ec2_handler.py
@@ -26,7 +26,7 @@ from cosmo_tester.framework.handlers import (
     BaseCloudifyInputsConfigReader)
 from cosmo_tester.framework.util import get_actual_keypath
 
-MANAGER_BLUEPRINT = 'ec2/ec2-manager-blueprint.yaml'
+MANAGER_BLUEPRINT = 'aws/aws-manager-blueprint.yaml'
 
 
 class EC2CleanupContext(BaseHandler.CleanupContext):
