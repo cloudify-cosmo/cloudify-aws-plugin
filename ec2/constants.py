c48b5395991291254b6281b32702ff6720bd1091
EarthmanT <earthmant@icloud.com>
2015-04-07 16:02:14 +0300

    Update constants.py

--- ec2/constants.py
+++ ec2/constants.py
@@ -49,8 +49,7 @@ KEYPAIR_REQUIRED_PROPERTIES = ['private_key_path']
 
 # config
 AWS_CONFIG_PROPERTY = 'aws_config'
-AWS_CONFIG_PATH_ENV_VAR = 'AWS_CONFIG_PATH'
-AWS_DEFAULT_CONFIG_PATH = '~/aws_config.json'
+AWS_DEFAULT_CONFIG_PATH = '~/.aws/credentials'
 EXTERNAL_RESOURCE_ID = 'aws_resource_id'
 NODE_INSTANCE = 'node-instance'
 RELATIONSHIP_INSTANCE = 'relationship-instance'
