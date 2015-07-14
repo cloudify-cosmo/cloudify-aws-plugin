4c370791a332bcd60795c4ce85497c1e9055bc5b
opencm <limor@gigaspaces.com>
2015-07-14 06:31:59 +0300

    Bump version to 1.2.1

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ setup(
     author='Gigaspaces',
     author_email='cosmo-admin@gigaspaces.com',
 
-    version='1.2',
+    version='1.2.1',
     description='Cloudify plugin for AWS infrastructure.',
 
     # This must correspond to the actual packages in the plugin.
@@ -33,7 +33,7 @@ setup(
 
     license='LICENSE',
     install_requires=[
-        'cloudify-plugins-common>=3.2',
+        'cloudify-plugins-common>=3.2.1',
         'boto==2.34.0'
     ]
 )
