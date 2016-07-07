5f6efcf0abaec64c3bdc402a3107dc89049423b7
Matt Wheeler <m@funkyhat.org>
2016-07-07 13:45:31 +0100

    Make the setup.py changes the same as geokala's

--- setup.py
+++ setup.py
@@ -40,7 +40,7 @@ setup(
     install_requires=[
         'cloudify-plugins-common>=3.3.1',
         'boto==2.38.0',
-        'boto3',
-        'pycrypto==2.6.1'
-    ]
+        'boto3--1.3.1',
+        'pycrypto==2.6.1',
+    ],
 )
