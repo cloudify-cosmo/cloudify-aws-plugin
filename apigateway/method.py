d7c78af400cbba690cb2fb2818c0c872f0d67f13
Matt Wheeler <m@funkyhat.org>
2016-07-13 00:51:43 +0100

    Got it to create an endpoint hurrah

--- apigateway/method.py
+++ apigateway/method.py
@@ -22,31 +22,43 @@ from .resource import get_parents
 
 lambda_uri_template = (
     "arn:aws:apigateway:{region}:lambda:path/"
-    "{api_version}/functions/{lambda_arn}/invocations")
+    "2015-03-31/functions/{lambda_arn}/invocations")
 api_uri_template = (
-    "arn:aws:execute-api:{region}:{account_id}:{api_id}/*/POST/DynamoDBManager"
+    "arn:aws:execute-api:{region}:{account_id}:{api_id}/*/"
+    "{http_method}{resource}"
     )
 
 
 def generate_lambda_uri(ctx, client, lambda_arn):
     return lambda_uri_template.format(
         region=client.meta.region_name,
-        api_version=client.meta.service_model.api_version,
         lambda_arn=lambda_arn,
         )
 
 
-def generate_api_uri(ctx, client, api_id):
+def generate_api_uri(ctx, client):
     account_id = ctx.target.instance.runtime_properties[
             'arn'].split(':')[4]
     # Only the account id field is all-digits
     assert int(account_id)
 
-    return api_uri_template.format(
+    parent, api = get_parents(ctx.source.instance)
+
+    resource_path = client.get_resource(
+        restApiId=api.runtime_properties['id'],
+        resourceId=parent.runtime_properties['resource_id'],
+        )['path']
+
+    uri = api_uri_template.format(
         region=client.meta.region_name,
         account_id=account_id,
-        api_id=api_id,
+        http_method=ctx.source.node.properties['http_method'],
+        resource=resource_path,
+        api_id=api.runtime_properties['id'],
         )
+
+    ctx.logger.info("api_uri: {}".format(uri))
+    return uri
 
 
 @operation
@@ -92,6 +104,7 @@ def get_connected_lambda(source, target):
     props = source.instance.runtime_properties
     linked = props.setdefault(
         'linked_lambdas', {})
+    # TODO: fix this in cloudify.manager.DirtyTrackingDict instead: setdefault
     props._set_changed()
     return linked.setdefault(target.node.name, {})
 
@@ -108,22 +121,41 @@ def connect_lambda(ctx):
         ctx, sclient,
         ctx.target.instance.runtime_properties['arn'],
         )
-    api_uri = generate_api_uri(
-        ctx, sclient,
-        api.runtime_properties['id'],
-        )
-    function_name = ctx.target.instance.runtime_properties['name']
 
     sclient.put_integration(
         restApiId=api.runtime_properties['id'],
         resourceId=parent.runtime_properties['resource_id'],
         type='AWS',
         httpMethod=sprops['http_method'],
-        integrationHttpMethod=sprops['http_method'],
+        integrationHttpMethod="POST",
         uri=lambda_uri,
         )
 
     runtime_props = get_connected_lambda(ctx.source, ctx.target)
+
+    sclient.put_integration_response(
+        restApiId=api.runtime_properties['id'],
+        resourceId=parent.runtime_properties['resource_id'],
+        httpMethod="POST",
+        statusCode="200",
+        selectionPattern=".*",
+        )
+
+    sclient.put_method_response(
+        restApiId=api.runtime_properties['id'],
+        resourceId=parent.runtime_properties['resource_id'],
+        httpMethod="POST",
+        statusCode="200",
+        )
+
+    sclient.create_deployment(
+        restApiId=api.runtime_properties['id'],
+        stageName='prod',
+        )
+
+    # Authorize the endpoint to call the lambda function
+    function_name = ctx.target.instance.runtime_properties['name']
+    api_uri = generate_api_uri(ctx, sclient)
 
     runtime_props['statement_id'] = '{}-{}'.format(
         ctx.source.node.name, ctx.target.node.name)
