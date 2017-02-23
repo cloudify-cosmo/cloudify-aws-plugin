75cf2c5c39f395e0b2b9c156b44bd3c86363dbe3
earthmant <earthmant@icloud.com>
2017-02-23 14:34:32 +0200

    updating version to 1.4.4.dev2

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ setup(
     author='Gigaspaces',
     author_email='cosmo-admin@gigaspaces.com',
 
-    version='1.4.4.dev1',
+    version='1.4.4.dev2',
     description='Cloudify plugin for AWS infrastructure.',
 
     # This must correspond to the actual packages in the plugin.
