4fc71ffabc6c6a4c64f0c0ab2449f43c4b30a0f4
Jessie Brami <jessie@gigaspaces.com>
2017-05-10 13:00:17 +0300

    Add support for attaching existing volume to instance

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ setup(
     author='Gigaspaces',
     author_email='cosmo-admin@gigaspaces.com',
 
-    version='1.4.7',
+    version='1.4.8',
     description='Cloudify plugin for AWS infrastructure.',
 
     # This must correspond to the actual packages in the plugin.
