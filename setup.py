b5f65a80019b47c1c665a0fe968327d7b12e81cf
Ahmad Musa <ahmad@cloudify.co>
2020-03-10 09:58:22 +0200

    CY-2266-Support-EKS-ChangeLog

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ from setuptools import find_packages
 
 setup(
     name='cloudify-aws-plugin',
-    version='2.3.0',
+    version='2.3.1',
     author='Cloudify Platform Ltd.',
     author_email='hello@cloudify.co',
     license='LICENSE',
