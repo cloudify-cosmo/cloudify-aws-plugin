ff7f017e04cfd3905deb9e9d726cddc25c0ba2a9
AdarShaked <59121240+AdarShaked@users.noreply.github.com>
2020-07-09 09:35:36 +0300

    Cy 3060 use bundle (#299)
    * use bundle and fix docker image for 5.1 integration tests
    
    * test integration tests
    
    * return privileged conf to docker of integration tests
    
    * no need to bump version

--- .circleci/test_examples.py
+++ .circleci/test_examples.py
@@ -22,35 +22,12 @@ from ecosystem_tests.dorkl import (
     cleanup_on_failure, prepare_test
 )
 
-'''Temporary until all the plugins in the bundle will 
-released with py2py3 wagons'''
-UT_VERSION = '1.23.5'
-UT_WAGON = 'https://github.com/cloudify-incubator/cloudify-utilities-plugin/' \
-           'releases/download/{v}/cloudify_utilities_plugin-{v}-centos' \
-           '-Core-py27.py36-none-linux_x86_64.wgn'.format(v=UT_VERSION)
-UT_PLUGIN = 'https://github.com/cloudify-incubator/cloudify-utilities-' \
-            'plugin/releases/download/{v}/plugin.yaml'.format(v=UT_VERSION)
-AN_VERSION = '2.9.2'
-AN_WAGON = 'https://github.com/cloudify-cosmo/cloudify-ansible-plugin/' \
-           'releases/download/{v}/cloudify_ansible_plugin-{v}-centos-' \
-           'Core-py27.py36-none-linux_x86_64.wgn'.format(v=AN_VERSION)
-AN_PLUGIN = 'https://github.com/cloudify-cosmo/cloudify-ansible-plugin' \
-            '/releases/download/{v}/plugin.yaml'.format(v=AN_VERSION)
-K8S_VERSION = '2.8.2'
-K8S_WAGON = 'https://github.com/cloudify-cosmo/cloudify-kubernetes-plugin/' \
-            'releases/download/{v}/cloudify_kubernetes_plugin-{v}-' \
-            'centos-Core-py27.py36-none-linux_x86_64.wgn'.format(v=K8S_VERSION)
-K8S_PLUGIN = 'https://github.com/cloudify-cosmo/cloudify-kubernetes-plugin/' \
-             'releases/download/{v}/plugin.yaml'.format(v=K8S_VERSION)
-PLUGINS_TO_UPLOAD = [(UT_WAGON, UT_PLUGIN), (AN_WAGON, AN_PLUGIN),
-                     (K8S_WAGON, K8S_PLUGIN)]
 SECRETS_TO_CREATE = {
     'aws_access_key_id': False,
     'aws_secret_access_key': False,
 }
 
-prepare_test(plugins=PLUGINS_TO_UPLOAD, secrets=SECRETS_TO_CREATE,
-             execute_bundle_upload=False)
+prepare_test(secrets=SECRETS_TO_CREATE)
 
 blueprint_list = ['examples/blueprint-examples/hello-world-example/aws.yaml',
                   'examples/blueprint-examples/'
