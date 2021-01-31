219a7a057b5fd1b3eb8fc55ac0f27cb2bbf3d2b6
earthmant <trammell@cloudify.co>
2021-01-31 12:44:58 +0200

    __NODOCS__

--- setup.py
+++ setup.py
@@ -12,8 +12,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-"""Cloudify plugin package config"""
-
 import os
 from setuptools import setup
 from setuptools import find_packages
