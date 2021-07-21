506547fe3276e192244217b6f39e972dc4dfd32f
EarthmanT <trammell@cloudify.co>
2021-07-21 16:56:14 -0400

    support spot fleet request

---
+++ cloudify_aws/ec2/resources/spot_fleet_request.py
@@ -0,0 +1,119 @@
+# Copyright (c) 2018 Cloudify Platform Ltd. All rights reserved
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#        http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+'''
+    EC2.VPC
+    ~~~~~~~~~~~~~~
+    AWS EC2 VPC interface
+'''
+# Third Party imports
+from botocore.exceptions import ClientError
+
+# Local imports
+from cloudify_aws.ec2 import EC2Base
+from cloudify_aws.common import decorators, utils
+
+RESOURCE_TYPE = 'EC2 Spot Fleet Request'
+SpotFleetRequest = 'SpotFleetRequest'
+SpotFleetRequests = 'SpotFleetRequests'
+SpotFleetRequestId = 'SpotFleetRequestId'
+SpotFleetRequestIds = 'SpotFleetRequestIds'
+SpotFleetRequestConfigs = 'SpotFleetRequestConfigs'
+
+
+class EC2SpotFleetRequest(EC2Base):
+    '''
+        EC2 Spot Fleet Request interface
+    '''
+    def __init__(self, ctx_node, resource_id=None, client=None, logger=None):
+        EC2Base.__init__(self, ctx_node, resource_id, client, logger)
+        self.type_name = RESOURCE_TYPE
+        self._properties = None
+
+    def get(self, spot_fleet_request_ids=None):
+        self.logger.debug(
+            'Getting list of {t}, max results page size is 123. '
+            'If a larger page size is required, please '
+            'open a ticket with Cloudify support.'.format(t=RESOURCE_TYPE))
+        spot_fleet_request_ids = spot_fleet_request_ids or [self.resource_id]
+        params = {SpotFleetRequestIds: spot_fleet_request_ids}
+        try:
+            return self.make_client_call(
+                'describe_spot_fleet_requests', params)
+        except ClientError:
+            return
+
+    @property
+    def properties(self):
+        '''Gets the properties of an external resource'''
+        if not self._properties:
+            resources = self.get()
+            if len(resources):
+                props = resources[SpotFleetRequestConfigs][0]
+                self._properties = props['SpotFleetRequestConfig']
+        return self._properties
+
+    @property
+    def status(self):
+        '''Gets the status of an external resource'''
+        if self.properties:
+            return self.properties['SpotFleetRequestState']
+
+    def create(self, params):
+        '''
+            Create a new AWS EC2 Spot Fleet Request.
+        '''
+        return self.make_client_call('request_spot_fleet', params)
+
+    def delete(self, params=None):
+        '''
+            Deletes an existing AWS EC2 Spot Fleet Request.
+        '''
+        return self.make_client_call('cancel_spot_fleet_requests', params)
+
+
+@decorators.aws_resource(EC2SpotFleetRequest, resource_type=RESOURCE_TYPE)
+def prepare(ctx, iface, resource_config, **_):
+    '''Prepares an AWS EC2 Vpc'''
+    # Save the parameters
+    ctx.instance.runtime_properties['resource_config'] = resource_config
+
+
+@decorators.aws_resource(EC2SpotFleetRequest, RESOURCE_TYPE)
+@decorators.wait_for_status(status_good=['active'],
+                            status_pending=['submitted'])
+@decorators.tag_resources
+def create(ctx, iface, resource_config, **_):
+    '''Creates an AWS EC2 Spot Fleet Request'''
+    params = utils.clean_params(
+        dict() if not resource_config else resource_config.copy())
+
+    # Actually create the resource
+    create_response = iface.create(params)
+    ctx.instance.runtime_properties['create_response'] = \
+        utils.JsonCleanuper(create_response).to_dict()
+
+    spot_fleed_request_id = create_response.get(SpotFleetRequestId, '')
+    iface.update_resource_id(spot_fleed_request_id)
+    utils.update_resource_id(ctx.instance, spot_fleed_request_id)
+
+
+@decorators.aws_resource(EC2SpotFleetRequest, RESOURCE_TYPE,
+                         ignore_properties=True)
+@decorators.untag_resources
+def delete(iface, resource_config, terminate_instances=False, **_):
+    '''Deletes an AWS EC2 Vpc'''
+    params = dict()
+    params.update({SpotFleetRequestIds: [iface.resource_id]})
+    params.update({'TerminateInstances': terminate_instances})
+    iface.delete(params)
---
+++ cloudify_aws/ec2/tests/test_spot_fleet_request.py
@@ -0,0 +1,135 @@
+# Copyright (c) 2018 Cloudify Platform Ltd. All rights reserved
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#        http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Standard imports
+import unittest
+
+# Third party imports
+from mock import patch, MagicMock
+
+# Local imports
+from cloudify_aws.common._compat import reload_module
+from cloudify_aws.ec2.resources import spot_fleet_request
+from cloudify_aws.common.tests.test_base import (
+    TestBase,
+    mock_decorator
+)
+from cloudify_aws.ec2.resources.spot_fleet_request import (
+    SpotFleetRequest,
+    SpotFleetRequestId,
+    SpotFleetRequestIds,
+    EC2SpotFleetRequest,
+)
+
+
+class TestEC2SpotFleetRequest(TestBase):
+
+    def setUp(self):
+        super(TestEC2SpotFleetRequest, self).setUp()
+        self.spot_fleet_request = EC2SpotFleetRequest(
+            "ctx_node", resource_id='foo', client=True, logger=None)
+        mock1 = patch('cloudify_aws.common.decorators.aws_resource',
+                      mock_decorator)
+        mock2 = patch('cloudify_aws.common.decorators.wait_for_status',
+                      mock_decorator)
+        mock1.start()
+        mock2.start()
+        reload_module(spot_fleet_request)
+
+    def test_class_properties(self):
+        value = {}
+        self.spot_fleet_request.client = self.make_client_function(
+            'describe_spot_fleet_requests', return_value=value)
+        res = self.spot_fleet_request.properties
+        self.assertIsNone(res)
+
+        value = {
+            'SpotFleetRequestConfigs': [
+                {
+                    'SpotFleetRequestConfig': {
+                        SpotFleetRequestId: 'foo'
+                    }
+                }
+            ]
+        }
+        self.spot_fleet_request.client = self.make_client_function(
+            'describe_spot_fleet_requests', return_value=value)
+        res = self.spot_fleet_request.properties
+        self.assertEqual(res[SpotFleetRequestId], 'foo')
+
+    def test_class_status(self):
+        value = {}
+        self.spot_fleet_request.client = self.make_client_function(
+            'describe_spot_fleet_requests', return_value=value)
+        res = self.spot_fleet_request.status
+        self.assertIsNone(res)
+
+        value = {
+            'SpotFleetRequestConfigs': [
+                {
+                    'SpotFleetRequestConfig': {
+                        SpotFleetRequestId: 'foo',
+                        'SpotFleetRequestState': 'active'
+                    }
+                }
+            ]
+        }
+        self.spot_fleet_request.client = self.make_client_function(
+            'describe_spot_fleet_requests', return_value=value)
+        res = self.spot_fleet_request.status
+        self.assertEqual(res, 'active')
+
+    def test_class_create(self):
+        value = {SpotFleetRequestId: 'foo'}
+        self.spot_fleet_request.client = self.make_client_function(
+            'request_spot_fleet', return_value=value)
+        res = self.spot_fleet_request.create(value)
+        self.assertEqual(res[SpotFleetRequestId], value[SpotFleetRequestId])
+
+    def test_class_delete(self):
+        params = {SpotFleetRequestIds: 'foo', 'TerminateInstances': True}
+        self.spot_fleet_request.client = self.make_client_function(
+            'cancel_spot_fleet_requests')
+        self.spot_fleet_request.delete(params)
+        self.assertTrue(
+            self.spot_fleet_request.client.cancel_spot_fleet_requests.called)
+
+    def test_prepare(self):
+        ctx = self.get_mock_ctx(SpotFleetRequest)
+        config = {'SpotFleetRequestConfig': {'foo': 'bar'}}
+        iface = MagicMock()
+        iface.create = self.mock_return(config)
+        spot_fleet_request.prepare(ctx, iface, config)
+        self.assertEqual(
+            ctx.instance.runtime_properties['resource_config'], config)
+
+    def test_create(self):
+        ctx = self.get_mock_ctx(SpotFleetRequest)
+        config = {'SpotFleetRequestConfig': {SpotFleetRequestId: 'foo'}}
+        self.spot_fleet_request.resource_id = \
+            config['SpotFleetRequestConfig'][SpotFleetRequestId]
+        iface = MagicMock()
+        iface.create = self.mock_return({SpotFleetRequestId: 'foo'})
+        spot_fleet_request.create(ctx=ctx, iface=iface, resource_config=config)
+        self.assertEqual(self.spot_fleet_request.resource_id, 'foo')
+
+    def test_delete(self):
+        ctx = self.get_mock_ctx(SpotFleetRequest)
+        iface = MagicMock()
+        spot_fleet_request.delete(ctx=ctx, iface=iface, resource_config={})
+        self.assertTrue(iface.delete.called)
+
+
+if __name__ == '__main__':
+    unittest.main()
