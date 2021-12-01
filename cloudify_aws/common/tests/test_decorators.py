b2ea8c506e0da1c51234dec27adc2e7a2605c5d6
kaplanyaniv <yaniv.224@gmail.com>
2021-12-01 14:56:14 +0200

    set max length of the maxDiff for full unittest log

--- cloudify_aws/common/tests/test_decorators.py
+++ cloudify_aws/common/tests/test_decorators.py
@@ -512,7 +512,7 @@ class TestDecorators(TestBase):
                          {'aws_resource_arn': 'res_id',
                           'aws_resource_id': 'aws_id',
                           'a': 'b',
-                          'resource_config': {'c': 'd'}})
+                          'resource_config': {}})
 
         mock_func.assert_not_called()
 
