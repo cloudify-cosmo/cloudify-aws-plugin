9b8b2249ae87f4f0799028f84330cacf72dda346
kaplanyaniv <yaniv.224@gmail.com>
2021-12-13 14:29:15 +0200

    added iface to test_prepare

--- cloudify_aws/ec2/tests/test_vpc_peering.py
+++ cloudify_aws/ec2/tests/test_vpc_peering.py
@@ -291,7 +291,7 @@ class TestEC2VpcPeering(TestBase):
 
     def test_prepare(self):
         ctx = self.get_mock_ctx("EC2VpcPeering")
-        iface = MagicMock
+        iface = MagicMock()
         vpc_peering.prepare(ctx, 'config', iface)
         self.assertEqual(
             ctx.instance.runtime_properties['resource_config'],
