a6ca3235d33dfaf1d7a4593b1ba814d9ae6c366b
Nelynehemia <nelyne1014@gmail.com>
2023-03-26 14:27:22 +0300

    add __version__ file

--- setup.py
+++ setup.py
@@ -13,6 +13,9 @@
 # limitations under the License.
 
 import os
+import re
+import pathlib
+
 from setuptools import setup
 from setuptools import find_packages
 
@@ -23,16 +26,13 @@ def read(rel_path):
         return fp.read()
 
 
-def get_version(rel_file='plugin.yaml'):
-    lines = read(rel_file)
-    for line in lines.splitlines():
-        if 'package_version' in line:
-            split_line = line.split(':')
-            line_no_space = split_line[-1].replace(' ', '')
-            line_no_quotes = line_no_space.replace('\'', '')
-            return line_no_quotes.strip('\n')
-    raise RuntimeError('Unable to find version string.')
+def get_version():
+    current_dir = pathlib.Path(__file__).parent.resolve()
 
+    with open(os.path.join(current_dir, 'cloudify_aws/__version__.py'),
+              'r') as outfile:
+        var = outfile.read()
+        return re.search(r'\d+.\d+.\d+', var).group()
 
 setup(
     name='cloudify-aws-plugin',
