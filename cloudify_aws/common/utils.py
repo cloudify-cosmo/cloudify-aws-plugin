a43799ccf69442d21d9bf816930fb88865c28dcb
Nelynehemia <nelyne1014@gmail.com>
2023-08-15 13:51:04 +0300

    add check vpc

--- cloudify_aws/common/utils.py
+++ cloudify_aws/common/utils.py
@@ -981,7 +981,7 @@ def delete_will_succeed(fn, params):
 
 
 def is_valid_aws_id(prefix, test_string):
-    pattern = r'^{}-\d\d\d\d\d\d\d\d\d$'.format(prefix)
+    pattern = r'^{}-[0-9a-f]{{17}}$'.format(re.escape(prefix))
     if re.match(pattern, test_string):
         return True
     else:
